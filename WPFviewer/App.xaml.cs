fae83e4f9f1e6d44ca1e84ef482e28cab5278595
Patrick Gallot <pgallot@datalogics.com>
2024-12-26 12:04:11 -0600

    A Simple WPF-based APDFL viewer.

---
+++ WPFviewer/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFviewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            // if (e.Args.Length == 0) return; // A way to examine Command-arguments.
+            for(int i = 0;i < e.Args.Length;i++) 
+                Console.WriteLine(e.Args[i]);
+
+        }
+    }
+}
